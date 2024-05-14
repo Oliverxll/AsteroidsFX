6efab0f5921a714981f2ef4cc9fc92507dc513a7
Oliverxll <bibern@live.dk>
2024-05-14 23:19:53 +0200

    JavaDoc

--- Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IEntityProcessingService.java
+++ Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IEntityProcessingService.java
@@ -5,13 +5,29 @@ import dk.sdu.mmmi.cbse.common.data.World;
 
 public interface IEntityProcessingService {
 
+
+
     /**
+     * <p>
+     *     The method is called every frame and is used to update the behaviour of the entities.
+     *     The order of the entities being processed is <b>NOT</b> guaranteed.
+     * </p>
      *
+     * <p>Pre-conditions:</p>
+     * <ul>
+     *     <li>{@code gameData} isn't {@code null}.</li>
+     *     <li>{@code world} isn't {@code null}.</li>
+     *     <li>{@code world} contains {@code entities} that implement {@code IEntityProcessingService}.</li>
      *
+     * </ul>
      *
-     * @param gameData
-     * @param world
-     * @throws
+     * <p>Post-conditions:</p>
+     * <ul>
+     *     <li>All {@code entities} has been processed in the {@code world}.</li>
+     * </ul>
+     *
+     * @param gameData GameData stores the metadata about the game’s state.
+     * @param world World stores the entities of the game.
      */
     void process(GameData gameData, World world);
 }
--- Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IGamePluginService.java
+++ Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IGamePluginService.java
@@ -5,7 +5,46 @@ import dk.sdu.mmmi.cbse.common.data.World;
 
 public interface IGamePluginService {
 
+    /**
+     * <p>
+     *     The method is called when the game starts.
+     *     The initial objects that the plugin is responsible for will be instantiated and put into the world.
+     * </p>
+     *
+     * <p>Pre-conditions:</p>
+     * <ul>
+     *     <li>{@code gameData} isn't {@code null}.</li>
+     *     <li>{@code world} isn't {@code null}.</li>
+     *     <li>{@code gameData} contains viewport dimensions.</li>
+     * </ul>
+     *
+     * <p>Post-conditions:</p>
+     * <ul>
+     *     <li>The {@code entity} has been instantiated and added to the world.</li>
+     * </ul>
+     *
+     * @param gameData GameData stores the metadata about the game’s state.
+     * @param world World stores the entities of the game.
+     */
     void start(GameData gameData, World world);
 
+    /**
+     * <p>The method is called when the game ends. Objects created by the plugins are responsible for deconstructing them as well.</p>
+     *
+     * <p>Pre-conditions:</p>
+     * <ul>
+     *     <li>{@code gameData} isn't {@code null}.</li>
+     *     <li>{@code world} isn't {@code null}.</li>
+     *     <li>{@code world} contains {@code Entities} that the plugin is responsible for.</li>
+     * </ul>
+     *
+     * <p>Post-conditions:</p>
+     * <ul>
+     *     <li>The entity has been removed from the world.</li>
+     * </ul>
+     *
+     * @param gameData GameData stores the metadata about the game’s state.
+     * @param world World stores the entities of the game.
+     */
     void stop(GameData gameData, World world);
 }
