3a8a33e8f96a0152981e3dbff3605bced049afa4
Oliverxll <bibern@live.dk>
2024-05-14 23:36:05 +0200

    Collision requirements using business rules

--- Collision/src/main/java/module-info.java
+++ Collision/src/main/java/module-info.java
@@ -1,6 +1,10 @@
 import dk.sdu.mmmi.cbse.common.services.IPostEntityProcessingService;
 
 module Collision {
-    requires Common;   
+    requires Common;
+    requires CommonPlayer;
+    requires CommonAsteroids;
+    requires CommonBullet;
+    requires CommonEnemy;
     provides IPostEntityProcessingService with dk.sdu.mmmi.cbse.collisionsystem.CollisionDetector;
 }
