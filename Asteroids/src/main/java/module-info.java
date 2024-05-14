f1830f7d11156b63f6dabc074002b603669c2e41
Oliverxll <bibern@live.dk>
2024-05-14 23:35:43 +0200

    Refactor

--- Asteroids/src/main/java/module-info.java
+++ Asteroids/src/main/java/module-info.java
@@ -1,3 +1,4 @@
+import dk.sdu.mmmi.cbse.asteroid.AsteroidControlSystem;
 import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
 import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
 
@@ -5,5 +6,5 @@ module Asteroid {
     requires Common;
     requires CommonAsteroids;
     provides IGamePluginService with dk.sdu.mmmi.cbse.asteroid.AsteroidPlugin;
-    provides IEntityProcessingService with dk.sdu.mmmi.cbse.asteroid.AsteroidProcessor;
+    provides IEntityProcessingService with AsteroidControlSystem;
 }
