7898d774f02635f7dda20ea0364e464418422ac5
Oliverxll <bibern@live.dk>
2024-05-15 09:34:43 +0200

    Bug fix

--- Enemy/src/main/java/dk/sdu/mmmi/cbse/enemysystem/EnemyControlSystem.java
+++ Enemy/src/main/java/dk/sdu/mmmi/cbse/enemysystem/EnemyControlSystem.java
@@ -1,12 +1,49 @@
 package dk.sdu.mmmi.cbse.enemysystem;
 
+import dk.sdu.mmmi.cbse.common.data.Entity;
 import dk.sdu.mmmi.cbse.common.data.GameData;
 import dk.sdu.mmmi.cbse.common.data.World;
+import dk.sdu.mmmi.cbse.common.enemy.Enemy;
 import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
 
+import java.util.Random;
+
 public class EnemyControlSystem implements IEntityProcessingService {
+
+    private int moveSpeed = 150;
+    private Random rand = new Random();
+
     @Override
     public void process(GameData gameData, World world) {
+        for (Entity enemy : world.getEntities(Enemy.class)) {
 
+            if (enemy.getHealth() <= 0) {
+                world.removeEntity(enemy);
+            }
+
+            double changeX = Math.cos(Math.toRadians(enemy.getRotation()));
+            double changeY = Math.sin(Math.toRadians(enemy.getRotation()));
+
+            enemy.setRotation(enemy.getRotation() + rand.nextInt(90));
+
+            enemy.setX(enemy.getX() + changeX * moveSpeed * gameData.getDeltaTime());
+            enemy.setY(enemy.getY() + changeY * moveSpeed * gameData.getDeltaTime());
+
+            if (enemy.getX() < 0) {
+                enemy.setX(enemy.getX() - gameData.getDisplayWidth());
+            }
+
+            if (enemy.getX() > gameData.getDisplayWidth()) {
+                enemy.setX(enemy.getX() % gameData.getDisplayWidth());
+            }
+
+            if (enemy.getY() < 0) {
+                enemy.setY(enemy.getY() - gameData.getDisplayHeight());
+            }
+
+            if (enemy.getY() > gameData.getDisplayHeight()) {
+                enemy.setY(enemy.getY() % gameData.getDisplayHeight());
+            }
+        }
     }
 }
